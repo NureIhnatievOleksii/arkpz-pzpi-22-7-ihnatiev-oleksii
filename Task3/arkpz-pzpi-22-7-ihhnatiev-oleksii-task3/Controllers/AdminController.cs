8b7329762ab9c11e67c8bd2db06ea008f7085076
Oleksii Ihnatiev <oleksii.ihnatiev@nure.ua>
2024-12-16 15:28:03 +0200

    Added comments

--- Task3/arkpz-pzpi-22-7-ihhnatiev-oleksii-task3/Controllers/AdminController.cs
+++ Task3/arkpz-pzpi-22-7-ihhnatiev-oleksii-task3/Controllers/AdminController.cs
@@ -1,6 +1,6 @@
-﻿using AirSense.Application.CQRS.Commands.Admins.AssignAdminRole;
-using AirSense.Application.CQRS.Commands.Admins.BanUser;
-using AirSense.Application.CQRS.Queries.Admins;
+using AirSense.Application.CQRS.Commands.Admins.AssignAdminRole; // Provides the AssignAdminRoleCommand functionality.
+using AirSense.Application.CQRS.Commands.Admins.BanUser; // Provides the BanUserCommand functionality.
+using AirSense.Application.CQRS.Queries.Admins; // Provides queries related to admin operations.
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,9 +11,10 @@ namespace AirSense.Api.Controllers
     public class AdminController(IMediator mediator) : ControllerBase
     {
         [HttpPost("assign-admin-role")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")] // Restricts access to users with the "Admin" role.
         public async Task<IActionResult> AssignAdminRole([FromBody] AssignAdminRoleCommand command, CancellationToken cancellationToken)
         {
+            // Assigns an admin role to a user based on the provided command.
             var result = await mediator.Send(command, cancellationToken);
 
             if (result.IsSuccess)
@@ -25,18 +26,21 @@ namespace AirSense.Api.Controllers
         }
 
         [HttpGet("user")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")] // Ensures only admins can retrieve user data.
         public async Task<IActionResult> GetAllUsers(CancellationToken cancellationToken)
         {
+            // Retrieves all users using the GetAllUsersQuery.
             var query = new GetAllUsersQuery();
             var users = await mediator.Send(query, cancellationToken);
 
             return Ok(users);
         }
+
         [HttpPost("ban/{userId}")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")] // Only admins can ban or unban users.
         public async Task<IActionResult> BanUser(Guid userId, [FromQuery] bool isBanned, CancellationToken cancellationToken)
         {
+            // Bans or unbans a user based on the isBanned parameter.
             var result = await mediator.Send(new BanUserCommand(userId, isBanned), cancellationToken);
 
             if (result)
