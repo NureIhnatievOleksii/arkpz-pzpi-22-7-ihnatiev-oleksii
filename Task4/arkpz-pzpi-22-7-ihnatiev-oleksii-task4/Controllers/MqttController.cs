af3d5d6547629828ad04e5ec96ea437ef786aff4
NureIhnatievOleksii <oleksii.ihnatiev@nure.ua>
2024-12-22 14:38:33 -0800

    Added task4

---
+++ Task4/arkpz-pzpi-22-7-ihnatiev-oleksii-task4/Controllers/MqttController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using AirSense.Domain.AirQualityAggregate;
+using AirSense.Application.Interfaces.Repositories;
+
+namespace AirSense.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MqttController : ControllerBase
+    {
+        private readonly MqttService _mqttService;
+
+        public MqttController(MqttService mqttService)
+        {
+            _mqttService = mqttService;
+        }
+
+        [HttpGet("last-message")]
+        public IActionResult GetLastMessage()
+        {
+            if (string.IsNullOrEmpty(_mqttService.LastMessage))
+            {
+                return NotFound("Немає отриманих повідомлень.");
+            }
+
+            return Ok(_mqttService.LastMessage);
+        }
+    }
+}
